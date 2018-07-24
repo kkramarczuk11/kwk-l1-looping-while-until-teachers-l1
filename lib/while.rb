727987ea99640dad69c891c5d3d9de0679aefd1c
kkramarczuk11 <kkramarczuk11@gmail.com>
2018-07-24 18:57:57 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,3 +1,14 @@
+def spell
+  levitationforce = 0
+while levitationforce == 10
 
+   puts "wingarium Leviosa"
+
+  levitationforce + 1
+ puts "#{levitationforce} move(s) completed!"
+ end 
+ end
+ 
+ spell
 
 
